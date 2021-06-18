30453a5fe607d4d9705bc2c7692dc2b1e95d3bc5
Digidolli <iamsadekaye@gmail.com>
2021-06-18 20:41:59 +0100

    Added relevant icons from API

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-let searchedCity = "tokyo";
+let searchedCity = "lisbon";
 let apiKey = "6b1dbcd0aa844738a2df75082ac70380";
 let searchedCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}&units=metric`;
 
@@ -21,6 +21,8 @@ function displayTemp(response){
     let humidityLevel = document.querySelector("#humidity");
     let windSpeed = document.querySelector("#wind");
     let day = document.querySelector("#day")
+    let weatherIcon = document.querySelector("#icon");
+    let iconId = response.data.weather[0].icon;
     celcTemp =  Math.round(celcTemp);
     displayTemp.innerHTML = `${celcTemp}°`;
     cityName.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
@@ -28,9 +30,8 @@ function displayTemp(response){
     humidityLevel.innerHTML = response.data.main.humidity;
     windSpeed.innerHTML = Math.round(response.data.wind.speed);
     day.innerHTML = formatDate(response.data.dt * 1000);
-    
-    
-    
+    weatherIcon.setAttribute ("src", `http://openweathermap.org/img/wn/${iconId}@2x.png`);
+    weatherIcon.setAttribute("alt", response.data.weather[0].description);
 };
 axios.get(searchedCityUrl).then(displayTemp);
 
